9fbf556b23dc413fe344802b99127e50e6fc2ed1
Yale Spector <yale.spector@gmail.com>
2022-12-24 11:33:19 -0500

    set default value for subscriber status in route

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -51,7 +51,7 @@ async fn insert_subscriber(
     new_subscriber: NewSubscriber,
 ) -> Result<(), sqlx::Error> {
     sqlx::query!(
-        "INSERT INTO subscriptions(id, email, name, subscribed_at) VALUES ($1, $2, $3, $4);",
+        "INSERT INTO subscriptions(id, email, name, subscribed_at, status) VALUES ($1, $2, $3, $4, 'confirmed');",
         Uuid::new_v4(),
         new_subscriber.email.as_ref(),
         new_subscriber.name.as_ref(),
